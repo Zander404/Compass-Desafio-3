6f336a0d147767466de20b0d91265979e9dc73ab
xandy <>
2025-03-06 16:28:08 -0400

    feat: implements Ticket and TicketRepository

--- ms-ticket-manager/src/main/java/com/zander404/msticketmanager/entities/Ticket.java
+++ ms-ticket-manager/src/main/java/com/zander404/msticketmanager/entities/Ticket.java
@@ -1,4 +1,32 @@
 package com.zander404.msticketmanager.entities;
 
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@Document
 public class Ticket {
+
+
+    @MongoId
+    UUID ticketId = UUID.randomUUID();
+
+    @CPF
+    String cpf;
+
+    String customerName;
+
+    @Email
+    String customerMail;
+
+    String eventId;
 }
